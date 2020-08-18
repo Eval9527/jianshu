6eb105b4e05a56fb7a47b758d24358357468e1c5
kailin <pooc95@163.com>
2020-08-18 19:43:24 +0800

    使用 style-components 及 reset.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style'
 import App from './App';
 
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
