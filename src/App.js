bd0433d4a68d0368831c197fae5d9a1d26accb5c
kailin <pooc95@163.com>
2020-08-28 17:06:11 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Header from "./common/header";
-import store from "./store";
-import {Provider} from 'react-redux'
+
 import {Globalstyle} from "./style";
 import {GlobalIconfont} from './statics/iconfont/iconfont'
 
 function App() {
   return (
-      <Provider store={store}>
+      <React.Fragment>
         <Globalstyle/>
         <GlobalIconfont/>
         <Header/>
-      </Provider>
+      </React.Fragment>
   );
 }
 
