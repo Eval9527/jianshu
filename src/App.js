794a68d4e97171dfb572636c81ca923c4b2af53e
kailin <pooc95@163.com>
2020-09-22 18:06:00 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "./common/header";
-
+import {BrowserRouter , Route} from "react-router-dom";
 import {Globalstyle} from "./style";
 import {GlobalIconfont} from './statics/iconfont/iconfont'
 
@@ -10,6 +10,10 @@ function App() {
         <Globalstyle/>
         <GlobalIconfont/>
         <Header/>
+        <BrowserRouter>
+          <Route path='/' exact render={() => <div>Home</div>}></Route>
+          <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+        </BrowserRouter>
       </React.Fragment>
   );
 }
