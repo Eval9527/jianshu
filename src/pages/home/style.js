ce7972d1f35bc1f1f01505205fa407c6f81241dd
Liu <pooc95@163.com>
2020-10-18 00:46:11 +0800

    首页文章列表制作

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -25,6 +25,7 @@ export const HomeRight = styled.div`
 export const TopicWrapper = styled.div`
   overflow: hidden;
   padding: 20px 0 10px 0;
+  margin-left: -18px;
 `
 
 export const TopicItem = styled.div`
@@ -45,5 +46,37 @@ export const TopicItem = styled.div`
     width: 32px;
     height: 32px;
     margin-right: 10px;
+  }
+`
+
+export const ListItem = styled.div`
+  overflow: hidden;
+  padding: 20px 0;
+  border-bottom: 1px solid #dcdcdc;
+  &:first-child {
+    border-top: 1px solid #dcdcdc;
+  }
+  .pic {
+    display: block;
+    width: 125px;
+    height: 100px;
+    float: right;
+    border-radius: 10px;
+  }
+`
+
+export const ListInfo = styled.div`
+  width: 500px;
+  float: left;
+  .title {
+    line-height: 27px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #333;
+  }
+  .desc {
+    line-height: 24px;
+    font-size: 13px;
+    color: #999;
   }
 `
