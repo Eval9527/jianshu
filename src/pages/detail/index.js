71dd7078acfdde681ab5eb26e6ff14be4df3e95c
Liu <pooc95@163.com>
2020-11-15 22:43:42 +0800

    使用 redux 管理详情页面数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 import { DetailWrapper, Header, Content} from "./style";
 
 
@@ -6,16 +7,18 @@ class Detail extends Component {
   render() {
     return (
       <DetailWrapper>
-        <Header>荒山独居两年得了抑郁症头发也掉光了</Header>
-        <Content>
-          <img src="https://tupian.sioe.cn/uploadfile/201110/17/1759255033.jpg" alt=""/>
-          <p>  <b>12年自己在江西南昌一个小乡镇租了一个猪场做起养猪人</b>，那猪场离村里有四公里左右，猪场周围都是荒山，还有很多坟堆，猪场下段有一个水库，平时偶尔有人来钓鱼，晚上很难见到一个人，平时我都是呆在猪场，没有必要的话我从不外出，当时买了一个无线网卡，每天就是上网。</p>
-          <p>  12年自己在江西南昌一个小乡镇租了一个猪场做起养猪人，那猪场离村里有四公里左右，猪场周围都是荒山，还有很多坟堆，猪场下段有一个水库，平时偶尔有人来钓鱼，晚上很难见到一个人，平时我都是呆在猪场，没有必要的话我从不外出，当时买了一个无线网卡，每天就是上网。</p>
-          <p>  12年自己在江西南昌一个小乡镇租了一个猪场做起养猪人，那猪场离村里有四公里左右，猪场周围都是荒山，还有很多坟堆，猪场下段有一个水库，平时偶尔有人来钓鱼，晚上很难见到一个人，平时我都是呆在猪场，没有必要的话我从不外出，当时买了一个无线网卡，每天就是上网。</p>
-        </Content>
+        <Header>{this.props.title}</Header>
+        <Content
+          dangerouslySetInnerHTML={{__html: this.props.content}}
+        />
       </DetailWrapper>
     )
   }
 }
 
-export default Detail
+const mapState = (state) => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content']),
+})
+
+export default connect(mapState, null)(Detail)
